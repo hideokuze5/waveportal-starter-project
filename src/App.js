13b85a84307a8768f93b5572ed291956a2d6b5c4
hideokuze5 <hideokuze88@protonmail.com>
2021-10-17 15:58:34 -0400

    refactor UI

--- src/App.js
+++ src/App.js
@@ -142,46 +142,147 @@ const wave = async () => {
   }, [allWaves])
   
   return (
-    <div className="mainContainer">
-      <div className="dataContainer">
-        <div className="header">
-        👋 Hey there!
-        </div>
-
-        <div className="bio">
-        I am Hideo Kuze and I am a cyber-network architect. Connect your Ethereum wallet and let us know you support us in our battle against Section 9!
-          <div style={{color: 'white', marginTop: '10px', fontWeight: 'bold'}}>
-          {waveCount !== '0' && 
-            'Thank you! You are supporter #' + waveCount + '. Your support will help us in our cause.'
-          }
+    <>
+      <div className="bg-white py-16 sm:py-24">
+            <div className="relative sm:py-16">
+              <div aria-hidden="true" className="hidden sm:block">
+                <div className="absolute inset-y-0 left-0 w-1/2 bg-gray-50 rounded-r-3xl" />
+                <svg className="absolute top-8 left-1/2 -ml-3" width={404} height={392} fill="none" viewBox="0 0 404 392">
+                  <defs>
+                    <pattern
+                      id="8228f071-bcee-4ec8-905a-2a059a2cc4fb"
+                      x={0}
+                      y={0}
+                      width={20}
+                      height={20}
+                      patternUnits="userSpaceOnUse"
+                    >
+                      <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
+                    </pattern>
+                  </defs>
+                  <rect width={404} height={392} fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)" />
+                </svg>
+              </div>
+              <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
+                <div className="relative rounded-2xl px-6 py-10 bg-gray-500 overflow-hidden shadow-xl sm:px-12 sm:py-20">
+                  <div aria-hidden="true" className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
+                    <svg
+                      className="absolute inset-0 h-full w-full"
+                      preserveAspectRatio="xMidYMid slice"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 1463 360"
+                    >
+                      <path
+                        className="text-indigo-500 text-opacity-40"
+                        fill="currentColor"
+                        d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
+                      />
+                      <path
+                        className="text-indigo-700 text-opacity-40"
+                        fill="currentColor"
+                        d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
+                      />
+                    </svg>
+                  </div>
+                  <div className="relative">
+                    <div className="sm:text-center">
+                      <h2 className="text-3xl font-extrabold text-white tracking-tight sm:text-4xl">
+                        👋 Hey there!
+                      </h2>
+                      <p className="mt-6 mx-auto max-w-2xl text-lg text-white">
+                        I am Hideo Kuze and I am a cyber-network architect.
+                      </p>
+                      <p className="mt-2 mx-auto max-w-2xl text-lg text-white">
+                        Connect your Ethereum wallet and support us in our battle against Section 9!
+                      </p>
+                      <p className="mt-2 mx-auto max-w-2xl text-lg text-indigo-200">
+                      {waveCount !== '0' && 
+                        'Thank you! You are supporter #' + waveCount + '. Your support will help us in our cause.'
+                      }
+                      </p>
+                      <p className="min-w-0 flex-1">
+                        {!currentAccount && (
+                          <button 
+                            className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10" 
+                            onClick={connectWallet}
+                          >
+                            Connect Wallet
+                          </button>
+                        )}
+                      </p>
+                    </div>
+                    <div className="mt-12 sm:mx-auto sm:max-w-lg sm:flex">
+                    
+                      <div className="min-w-0 flex-1">
+                        {/* <label htmlFor="cta-email" className="sr-only">
+                          Email address
+                        </label> */}
+                        <input
+                          id="message"
+                          type="text"
+                          className="block w-full border border-transparent rounded-md px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:outline-none focus:border-transparent focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600"
+                          placeholder="Enter your message of support"
+                          onChange={e => setMessage(e.target.value)}
+                        />
+                      </div>
+                      <div className="mt-4 sm:mt-0 sm:ml-3">
+                        <button
+                          type="submit"
+                          className="block w-full rounded-md border border-transparent px-5 py-3 bg-indigo-500 text-base font-medium text-white shadow hover:bg-indigo-400 focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-indigo-600 sm:px-10"
+                          onClick={wave}
+                        >
+                          Submit message
+                        </button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </div>
-        </div>
-
-        <input type="text" name="message" placeholder="Enter your message of support" onChange={e => setMessage(e.target.value)}/>
-
-        <button className="waveButton" onClick={wave}>
-          Submit message!
-        </button>
-        
-        {/*
-        * If there is no currentAccount render this button
-        */}
-        {!currentAccount && (
-          <button className="waveButton" onClick={connectWallet}>
-            Connect Wallet
-          </button>
-        )}
-
-        {allWaves.map((wave, index) => {
-          return (
-            <div key={index} style={{ backgroundColor: "white", marginTop: "16px", padding: "8px" }}>
-              <div style={{color: "black"}}>Address: {wave.address}</div>
-              <div style={{color: "black"}}>Time: {wave.timestamp.toString()}</div>
-              <div style={{color: "black"}}>Message: {wave.message}</div>
-            </div>)
-        })}
-      </div>
-    </div>
+          <div className="flex flex-col">
+            <div className="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+              <div className="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
+                <div className="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                  <table className="min-w-full divide-y divide-gray-200">
+                    <thead className="bg-gray-50">
+                      <tr>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Address
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Time
+                        </th>
+                        <th
+                          scope="col"
+                          className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider"
+                        >
+                          Message
+                        </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {allWaves.map((wave, index) => (
+                        <tr key={index} className={index % 2 === 0 ? 'bg-white' : 'bg-gray-50'}>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm font-medium text-gray-900">{wave.address}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{wave.timestamp.toString()}</td>
+                          <td className="px-6 py-4 whitespace-nowrap text-sm text-gray-500">{wave.message}</td>
+                        </tr>
+                      ))}
+                    </tbody>
+                  </table>
+                </div>
+              </div>
+            </div>
+          </div>
+    </>
   );
 }
 
